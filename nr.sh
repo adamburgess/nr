01f96a68b35831266b764023c5caa13d3dfa421c
Adam Burgess <adamburgess@users.noreply.github.com>
2021-03-19 12:25:10 +1100

    initial commit

---
+++ nr.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Find the nearest package.json.
+find_package_json()
+{
+    # if it exists here then stop
+    if [ -f "package.json" ]; then
+        return;
+    fi
+    # If we are at the root then fail
+    if [ "$PWD" = "/" ]; then
+        >&2 echo "package.json not found"
+        exit 1;
+    fi
+    # Go to parent and start again
+    cd ..
+    find_package_json
+}
+
+find_package_json
+
+# Run the script. If jq succeeds then cmd will hold it
+if cmd=$(jq .scripts.$1 -r -e package.json); then
+    # Add node_modules to path, and run the script + any arguments with bash
+    PATH=node_modules/.bin:$PATH bash -c "$cmd ${@:2}"
+else
+    >&2 echo "Script $1 not found"
+    exit 1
+fi
