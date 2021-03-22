bf3242b91ecf46c4335d097a6da00bc298bf9cb7
Adam Burgess <adamburgess@users.noreply.github.com>
2021-03-22 15:12:34 +1100

    fix issues with space/colon

--- nr.sh
+++ nr.sh
@@ -20,7 +20,7 @@ find_package_json()
 find_package_json
 
 # Run the script. If jq succeeds then cmd will hold it
-if cmd=$(jq .scripts.$1 -r -e package.json); then
+if cmd=$(jq .scripts.\""$1"\" -r -e package.json); then
     # Add node_modules to path, and run the script + any arguments with bash
     PATH=node_modules/.bin:$PATH bash -c "$cmd ${@:2}"
 else
