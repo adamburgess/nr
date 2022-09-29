5a2417d4e0dfd15ae316ca433f1463ba3156b536
Adam Burgess <adamburgess@users.noreply.github.com>
2022-09-29 18:39:06 +1000

    bash and spaces are too hard.

--- nr.sh
+++ nr.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+function cleanup {
+    rm $tmpfile
+}
+
 # Find the nearest package.json.
 find_package_json()
 {
@@ -18,11 +22,19 @@ find_package_json()
 }
 
 find_package_json
-
+shopt -s extglob
 # Run the script. If jq succeeds then cmd will hold it
 if cmd=$(jq .scripts.\""$1"\" -r -e package.json); then
     # Add node_modules to path, and run the script + any arguments with bash
-    PATH=node_modules/.bin:$PATH bash -c "$cmd ${@:2}"
+    PATH=node_modules/.bin:$PATH
+    tmpfile=$(mktemp /tmp/nr.XXXXXX)
+    trap cleanup EXIT
+    echo "#!/bin/bash" > $tmpfile
+    echo -n $cmd >> $tmpfile
+    echo -n " " >> $tmpfile
+    shift
+    echo "${@@Q}" >> $tmpfile
+    bash $tmpfile
 else
     >&2 echo "Script $1 not found"
     exit 1
